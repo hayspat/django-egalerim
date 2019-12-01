bde7c3102c006c717b1ab77088250678e50e49f4
Faruk Aydın <faruk@faruk-arch.localdomain>
2019-12-01 21:13:43 +0300

    eslint ayarı

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { useEffect } from "react";
-import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
 import { connect } from "react-redux";
 import BaseRouter from "./routes";
 import * as actions from "./store/actions/auth";
